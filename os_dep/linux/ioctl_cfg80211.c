27684cee1dcbf988a1d07fd2c2b60f707ccabc20
morrownr <morrownr@gmail.com>
2023-11-23 08:01:24 -0600

    repair support for kernel 6.7

--- os_dep/linux/ioctl_cfg80211.c
+++ os_dep/linux/ioctl_cfg80211.c
@@ -5322,7 +5322,7 @@ static int cfg80211_rtw_add_beacon(struct wiphy *wiphy, struct net_device *ndev,
 	rtw_mi_scan_abort(adapter, _TRUE);
 	rtw_mi_buddy_set_scan_deny(adapter, 300);
 #if (LINUX_VERSION_CODE >= KERNEL_VERSION(6, 7, 0))
-	ret = rtw_add_beacon(adapter, info->beacon.head, info->beacon.head_len, info->beacon.tail, info->beacon.tail_len);
+	ret = rtw_add_beacon(adapter, ap->beacon.head, ap->beacon.head_len, ap->beacon.tail, ap->beacon.tail_len);
 #else
 	ret = rtw_add_beacon(adapter, info->head, info->head_len, info->tail, info->tail_len);
 #endif
@@ -5410,7 +5410,7 @@ exit:
 
 static int cfg80211_rtw_change_beacon(struct wiphy *wiphy, struct net_device *ndev,
 #if (LINUX_VERSION_CODE >= KERNEL_VERSION(6, 7, 0))
-		struct cfg80211_ap_update *info)
+		struct cfg80211_ap_update *ap)
 #else
 		struct cfg80211_beacon_data *info)
 #endif		
